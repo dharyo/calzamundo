8aa2c6cb355fcf4c6b6dfbf63bfc7e7077df5f58
HernanCarrillo <darionita@gmail.com>
2022-10-31 18:41:46 -0500

    Commit 6 - ajuste clases de servicio

--- src/main/java/com/calzamundoshoes/calzamundo/controller/CalzamundoController.java
+++ src/main/java/com/calzamundoshoes/calzamundo/controller/CalzamundoController.java
@@ -1,8 +1,16 @@
 package com.calzamundoshoes.calzamundo.controller;
 
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//import com.calzamundoshoes.calzamundo.service.CalzadoService;
+
 public class CalzamundoController {
+
+    //@Autowired
+    //private CalzadoService calzadoService;
+   
+
     @RequestMapping("/")
     public String principal() {
         return "index";

cf042bb8434363664c924b1606382c988b8b5285
HernanCarrillo <darionita@gmail.com>
2022-11-18 02:22:31 -0500

    commit 10 - Spring Security

--- src/main/java/com/calzamundoshoes/calzamundo/SecurityConfig/WebSecurityConfig.java
+++ src/main/java/com/calzamundoshoes/calzamundo/SecurityConfig/WebSecurityConfig.java
@@ -3,10 +3,13 @@ package com.calzamundoshoes.calzamundo.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.calzamundoshoes.calzamundo.service.UserService;
 
 @Configuration
 @EnableWebSecurity
@@ -21,8 +24,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
         http
             .authorizeRequests()
 	        .antMatchers(resources).permitAll()  
-	        .antMatchers("/","/index").permitAll()
-	        .antMatchers("/Home*").access("hasRole('user')")
+	        .antMatchers("/","/index","/login").permitAll()
+	        .antMatchers("/inventory*").access("hasRole('ADMIN')")
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
@@ -50,7 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     }
 	
     @Autowired
-    UserDetailsServiceImpl userDetailsService;
+    UserService userService;
 	
     //Registra el service para usuarios y el encriptador de contrasena
     @Autowired
@@ -58,6 +61,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
  
         // Setting Service to find User in the database.
         // And Setting PassswordEncoder
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());     
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());     
     }
 }
